61c6dcbee0368bcc3c60c386a2cbdc3de0ff9229
Jason <jason.pu19@outlook.com>
2021-10-11 21:50:15 -0400

    Create calendar.py
    Created the Calendar class and its constructor

---
+++ calendar.py
@@ -0,0 +1,6 @@
+class Calendar:
+    def __init__(self, event, date, start_time, end_time):
+        self.event = event
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
