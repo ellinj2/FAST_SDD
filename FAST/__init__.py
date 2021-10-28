d6eb5c362d50e552cda283dda64134b9c55de7ec
JesseEllin <iamjesseellin@gmail.com>
2021-10-28 13:14:03 -0400

    Trying to fix __init__

--- FAST/__init__.py
+++ FAST/__init__.py
@@ -13,20 +13,20 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
 
     # Application Configuration
-    app.config.from_object('config.Config')
+    # app.config.from_object('config.Config')
 
     # Initialize Plugins
     db.init_app(app)
     login_manager.init_app(app)
 
     with app.app_context():
-        from . import routes
-        from . import auth
-        from .assets import compile_assets
+        # from . import routes
+        # from . import auth
+        # from .assets import compile_assets
 
-        # Register Blueprints
-        app.register_blueprint(routes.main_bp)
-        app.register_blueprint(auth.auth_bp)
+        # # Register Blueprints
+        # app.register_blueprint(routes.main_bp)
+        # app.register_blueprint(auth.auth_bp)
 
         # Create Database Models
         db.create_all()
@@ -36,3 +36,5 @@ def create_app():
             compile_assets(app)
 
         return app
+
+app = create_app()
---
+++ app.py
@@ -0,0 +1,4 @@
+from FAST import app, db
+
+if __name__ == "__main__":
+	app.run(debug=True)
