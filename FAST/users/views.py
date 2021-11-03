1d6f0a661aba4b2b19a57fbfcacd407fc7784ad7
JesseEllin <iamjesseellin@gmail.com>
2021-11-03 12:17:17 -0400

    Moved user views to directory

---
+++ FAST/users/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from flask_login import current_user, login_required, logout_user
+from FAST import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+""" User homepage / dashboard """
+@app.route('/dashboard', methods =['GET'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
