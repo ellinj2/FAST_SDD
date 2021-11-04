8db1fc546afae249df313556fffe4ecb9648967f
JesseEllin <iamjesseellin@gmail.com>
2021-11-03 23:54:18 -0400

    Updated functions to display forms

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -1,12 +1,14 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user, login_required, logout_user
 from FAST import app
+from FAST.calendar.forms import *
 
 calendar = Blueprint("calendar", __name__)
 
 @calendar.route('/upload_event', methods=["GET", "POST"])
 def upload_event():
-	return render_template("upload_event.html")
+	form = EventForm()
+	return render_template("upload_event.html", form=form)
 
 @calendar.route('/generate_claendar', methods=["GET", "POST"])
 def generate_calendar():
@@ -15,3 +17,7 @@ def generate_calendar():
 @calendar.route('/download_calendar', methods=["GET", "POST"])
 def download_calendar():
 	return render_template("download_calendar.html")
+
+@calendar.route('/help')
+def help():
+	return redirect(url_for('calendar.upload_event'))
