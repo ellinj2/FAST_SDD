4ae1d601a1ce65b76ed2f47255f83389314d8791
JesseEllin <iamjesseellin@gmail.com>
2021-11-03 23:32:52 -0400

    Updated routes to handle Blueprints

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -3,3 +3,15 @@ from flask_login import current_user, login_required, logout_user
 from FAST import app
 
 calendar = Blueprint("calendar", __name__)
+
+@calendar.route('/upload_event', methods=["GET", "POST"])
+def upload_event():
+	return render_template("upload_event.html")
+
+@calendar.route('/generate_claendar', methods=["GET", "POST"])
+def generate_calendar():
+	return render_template("generate_calendar.html")
+
+@calendar.route('/download_calendar', methods=["GET", "POST"])
+def download_calendar():
+	return render_template("download_calendar.html")
--- FAST/users/views.py
+++ FAST/users/views.py
@@ -1,17 +1,27 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user, login_required, logout_user
 from FAST import app
+from FAST.users.forms import *
 
 users = Blueprint("users", __name__)
 
 """ User homepage / dashboard """
-@app.route('/dashboard', methods =['GET'])
+@users.route('/dashboard', methods =['GET'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/logout')
+@users.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for("index"))
+
+@users.route('/register')
+def register():
+    form = SignupForm()
+    return render_template("register.html", form=form)
+
+@users.route('/login')
+def login():
+    return render_template("login.html")
