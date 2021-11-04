371d22c724f211cbebcd5c66e72337e9baa8d9f9
JesseEllin <iamjesseellin@gmail.com>
2021-11-04 02:31:57 -0400

    Fixed form handling errors

--- FAST/users/views.py
+++ FAST/users/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user, login_required, logout_user
-from FAST import app
+from FAST import app, db
 from FAST.users.forms import *
+from FAST.database import User
 
 users = Blueprint("users", __name__)
 
@@ -17,12 +18,21 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
-@users.route('/register')
+@users.route('/register', methods=["GET", "POST"])
 def register():
     form = SignupForm()
+
+    if form.validate_on_submit():
+        print(f"Registering {form.email.data}\n{form.password.data}")
+        user = User(email=form.email.data,
+                    password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template("register.html", form=form)
 
-@users.route('/login')
+@users.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template("login.html", form=form)
