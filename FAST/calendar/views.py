825e95c12abc8ae754d62af86b25770e7f94a777
JesseEllin <iamjesseellin@gmail.com>
2021-11-07 01:04:59 -0500

    Added Event form handling

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -2,19 +2,35 @@ from flask import render_template, Blueprint, redirect, url_for
 from flask_login import current_user, login_required, logout_user
 from FAST import app
 from FAST.calendar.forms import *
+from FAST.calendar.models import *
+from FAST.database import *
 
 calendar = Blueprint("calendar", __name__)
 
 @calendar.route('/upload_event', methods=["GET", "POST"])
+@login_required
 def upload_event():
 	form = EventForm()
+
+	if form.validate_on_submit():
+		event = EventObject(tag=form.tag.data)
+		event_entry = Event(name=event.tag,
+							obj=event,
+							user_id=current_user.id)
+
+		db.session.add(event_entry)
+		db.session.commit()
+		form = EventForm()
+		return render_template("upload_event.html", form=form)
 	return render_template("upload_event.html", form=form)
 
 @calendar.route('/generate_claendar', methods=["GET", "POST"])
+@login_required
 def generate_calendar():
 	return render_template("generate_calendar.html")
 
 @calendar.route('/download_calendar', methods=["GET", "POST"])
+@login_required
 def download_calendar():
 	return render_template("download_calendar.html")
 
