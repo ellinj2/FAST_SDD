33f1c2283ad9b879c1e94e7a59be428ae61494ff
JesseEllin <iamjesseellin@gmail.com>
2021-11-11 17:28:28 -0500

    Fixed login issue

--- FAST/users/views.py
+++ FAST/users/views.py
@@ -38,6 +38,13 @@ def register():
 @users.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for("index"))
     return render_template("login.html", form=form)
 
 @users.route("/view_events", methods=["GET"])
