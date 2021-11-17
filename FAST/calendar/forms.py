46ee12d7317eedf11566dafd7b17f4238bb3ec5a
JesseEllin <iamjesseellin@gmail.com>
2021-11-17 15:25:13 -0500

    Fixed typos

--- FAST/calendar/forms.py
+++ FAST/calendar/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import (
     DataRequired,
 )
