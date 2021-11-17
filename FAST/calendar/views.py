03e7b69e042dcf21daca4b402ffc1e40441695fc
Jason <jason.pu19@outlook.com>
2021-11-17 10:18:24 -0500

    Created button for choose file and upload

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, Blueprint, redirect, url_for
+from flask import render_template, Blueprint, redirect, url_for, request, flash
 from flask_login import current_user, login_required, logout_user
 from FAST import app
 from FAST.calendar.forms import *
 from FAST.calendar.models import *
 from FAST.database import *
+from werkzeug.utils import secure_filename
+import os
 
 calendar = Blueprint("calendar", __name__)
 
@@ -38,3 +40,30 @@ def download_calendar():
 @calendar.route('/help')
 def help():
 	return redirect(url_for('calendar.upload_event'))
+
+@calendar.route('/upload_file', methods=["GET", "POST"])
+def upload_file():
+	if request.method == 'POST':
+	# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# If the user does not select a file, the browser submits an
+		# empty file without a filename.
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file:
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('download_file', name=filename))
+	return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
