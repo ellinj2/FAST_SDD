23ddff1e943917c602a6ab284ff834938db92bf8
JesseEllin <iamjesseellin@gmail.com>
2021-11-17 14:53:54 -0500

    Added ClusterForm

--- FAST/calendar/forms.py
+++ FAST/calendar/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import (
     DataRequired,
 )
@@ -9,3 +9,11 @@ class EventForm(FlaskForm):
     information = TextAreaField("Description", validators=[DataRequired()])
 
     submit = SubmitField("Create Event")
+
+class ClusterForm(FlaskForm):
+    attribute = SelectField("Clustering Attribute", validators=[DataRequired()])
+    shift = IntegerField("Time slots between Events")
+    start = SelectField("Starting time method")
+    centers = IntegerField("Number of clusters")
+
+    submit = SubmitField("Cluster")
