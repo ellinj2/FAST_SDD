0e1c34bdb69b850bf0d116667c714a75db29947d
Jason <jason.pu19@outlook.com>
2021-11-17 21:13:44 -0500

    Created the template for uploading event

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -42,28 +42,11 @@ def help():
 	return redirect(url_for('calendar.upload_event'))
 
 @calendar.route('/upload_file', methods=["GET", "POST"])
+@login_required
 def upload_file():
 	if request.method == 'POST':
-	# check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		# If the user does not select a file, the browser submits an
-		# empty file without a filename.
-		if file.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if file:
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('download_file', name=filename))
-	return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+		req = request.get_json()
+		language = req['language']
+		print(language)
+		print("here")
+	return render_template("upload_file.html")
