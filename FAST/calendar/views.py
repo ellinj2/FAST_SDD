afc6f61e1f108e9f283aea670659d3e76464f718
Jason <jason.pu19@outlook.com>
2021-11-17 23:11:42 -0500

    Experimented with WTForm

--- FAST/calendar/views.py
+++ FAST/calendar/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, redirect, url_for, request, flash
+from FAST.forms import DataUploadForm
 from flask_login import current_user, login_required, logout_user
 from FAST import app
 from FAST.calendar.forms import *
@@ -44,9 +45,37 @@ def help():
 @calendar.route('/upload_file', methods=["GET", "POST"])
 @login_required
 def upload_file():
-	if request.method == 'POST':
-		req = request.get_json()
-		language = req['language']
-		print(language)
-		print("here")
-	return render_template("upload_file.html")
+	form = EventForm()
+	print("here1")
+	print(form.data)
+	print(form.information)
+	# if form.validate_on_submit():
+	# 	print("here2")
+	# 	notes = [[f.strip() for f in line.split(':')] for line in form.information.data.split('\n')]
+	# 	notes = {note[0]: ':'.join(note[1:]) for note in notes}
+	# 	print(notes)
+	# 	event = EventObject(tag=form.tag.data,
+	# 						start_time=notes["Start Time"],
+	# 				 		end_time=notes["End Time"])
+	# 	event.assign(**notes)
+	# 	event_entry = Event(name=event.tag,
+	# 						obj=event,
+	# 						user_id=current_user.id)
+
+	# 	db.session.add(event_entry)
+	# 	db.session.commit()
+	# 	form = EventForm()
+	return render_template("upload_file.html", form=form)
+	# if form.validate_on_submit():
+    #     filename = secure_filename(form.file.data.filename)
+    #     form.file.data.save('uploads/' + filename)
+    #     return redirect(url_for('upload'))
+
+
+	# if request.method == 'POST':
+		# req = request.get_json()
+		# form = DataUploadForm.from_json(req)
+		# language = req['language']
+		# print(language)
+	# 	print("here")
+	# return render_template("upload_file.html")
